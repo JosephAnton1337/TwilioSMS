0c31cb69e61d985a3b502e531df611fabdbd2a36
Pavel Rudnev <prudnev@ippon.fr>
2022-02-28 23:28:27 +0300

    drop spaces

--- src/main/java/com/twilio/demo/TwilioSmsApplication.java
+++ src/main/java/com/twilio/demo/TwilioSmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwilioSmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwilioSmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TwilioSmsApplication.class, args);
+    }
 
 }
